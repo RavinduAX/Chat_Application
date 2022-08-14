e7b5bcb5af518c0848f258cd145218bb387c1534
RavinduAX <ravinduadassuriya59@gmail.com>
2022-08-14 21:47:39 +0530

    Created Navigations

--- src/controller/ChatRoomFormController.java
+++ src/controller/ChatRoomFormController.java
@@ -1,5 +1,28 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ChatRoomFormController {
 
+    public JFXTextField txtMassage;
+    public ScrollPane scrlPane;
+    public VBox vBox;
+    public AnchorPane contextChatRoom;
+
+    public void btnLeaveOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) contextChatRoom.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,5 +1,30 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
 
+    public JFXTextField txtUserName;
+    public AnchorPane contextLogin;
+
+    public void btnLogInOnAction(ActionEvent actionEvent) throws IOException {
+        String userName = txtUserName.getText();
+        System.out.println(userName);
+
+//        Stage stage = new Stage();
+//        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ChatRoomForm.fxml"))));
+//        stage.setTitle("Chat Room");
+//        stage.show();
+
+        Stage stage = (Stage) contextLogin.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ChatRoomForm.fxml"))));
+
+    }
 }
